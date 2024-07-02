de89074e31282a08eb7d302dd87c85dd09500e96
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-07-02 09:55:42 -0300

    Add files via upload

---
+++ 2-media-foreach.js
@@ -0,0 +1,11 @@
+//forEasch é um método e dentro do parâmetro dele 
+//passaremos uma outra função para esse metódo
+//Podemos escrever uma função anônima do JS.
+const notas=[1,2,3,5]
+notas.forEach(function(){})
+
+notas.forEach(function (){
+    console.log("oi")
+});
+//Foi impresso 
+
