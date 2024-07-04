c57596b06248075ec8b04b9a7c456f8f94c58cb6
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-07-04 11:03:51 -0300

    Add files via upload

---
+++ 6-callback.js
@@ -0,0 +1,9 @@
+const nomes = ["Evaldo", "Mari", "Camis"];
+
+ nomes.forEach(function (nome){
+ console.log (nome)
+ })
+ //modo mais ultilizado = arrow function (flecha
+  nomes.forEach((nome)=>{
+    console.log(nome)
+  })
