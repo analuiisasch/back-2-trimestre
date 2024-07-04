2a641f92c154008ede0c15dce331fe9309d2b669
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-07-04 11:37:55 -0300

    Add files via upload

---
+++ 9.foreach.js
@@ -0,0 +1,9 @@
+const lista = [1, 2, 3, 4, 5]
+let soma = 0;
+lista.forEach(numero => soma += numero);
+console.log(soma) //15
+// um pouco maislegivel
+lista.forEach(numero => {
+ soma = soma + numero
+});
+console.log(soma)
